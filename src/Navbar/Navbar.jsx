79814dc2bbe970667e364ca2713e8af870825edd
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-08-04 01:09:03 +0800

    fix navbar button issues

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,33 +1,26 @@
+// Navbar.js
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css'; // Mengimpor CSS kustom
-// import logo from './assets/image/head-image.png'; // Pastikan Anda memiliki gambar ini di direktori yang benar
+import './Navbar.css';
 
-function Navbar(props) {
+function CustomNavbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand d-flex align-items-center" href="#">
-                <img src={props.logo} alt="logo" height="40" width="40" className="d-inline-block align-top" />
-                <span>Rayhan Kimi</span>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="https://github.com/rayhankimi">GitHub</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="https://www.linkedin.com/in/rayhan-kimi-nabiel-athallah-897721245/">LinkedIn</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="https://open.spotify.com/user/5prfrj9o5sga74t50nzigx4ps">Spotify</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Navbar.Brand href="#">
+                <img src={props.logo} alt="logo" height="40" width="40" className="d-inline-block align-top" id="brand-logo"/>
+                <span id="brand-text">Rayhan Kimi</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarNav" />
+            <Navbar.Collapse id="navbarNav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="https://github.com/rayhankimi">GitHub</Nav.Link>
+                    <Nav.Link href="https://www.linkedin.com/in/rayhan-kimi-nabiel-athallah-897721245/">LinkedIn</Nav.Link>
+                    <Nav.Link href="https://open.spotify.com/user/5prfrj9o5sga74t50nzigx4ps">Spotify</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default CustomNavbar;
